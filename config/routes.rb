7cea4d813b8c9348503a032c2cc2d230fff15f27
Christian LIM <christian.lim@outlook.fr>
2017-12-16 18:16:06 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'rachid/pute'
+
+  get 'pages/investir'
+
+  devise_for :users
+  get 'home/Index'
+  get 'pages/home'
+  get 'inscription', to: 'home#Index'
+  get 'investir', to: 'pages#investir'
+  get 'about', to: 'pages#about'
+  resources :contacts, only: [:new, :create]
+  resources :charges
+  root 'pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
