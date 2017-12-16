65d481cbd80fbbf5d40779e12dc942a78303adcd
Christian LIM <christian.lim@outlook.fr>
2017-12-16 18:21:08 +0100

    X

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://young-crag-53838.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://mysterious-brushlands-50185.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'https://young-crag-53838.herokuapp.com',
+  :domain => 'https://mysterious-brushlands-50185.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
